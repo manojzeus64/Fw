bcaf87bad8990ff40160f51dcbb8f65f6c8dad08
manojzeus64 <148335260+manojzeus64@users.noreply.github.com>
2023-11-02 10:49:31 +0530

    Add files via upload
    Done

---
+++ Fw.java
@@ -0,0 +1,15 @@
+package com.frames;
+
+
+
+public class Fw {
+	
+	public static void main(String[] args) {
+		System.out.println("Manoj");
+                System.out.println("java");
+                System.out.println("student
+")
+                
+	}
+
+}
